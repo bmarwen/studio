f79f190a1db830b202944920fb411c7264c6264d
Marwen Boulabiar <gmarwoin@gmail.com>
2025-07-15 10:05:29 +0000

    the arrows of both class and race please remove the border on the arrow

--- src/components/game/CharacterCreator.tsx
+++ src/components/game/CharacterCreator.tsx
@@ -177,8 +177,8 @@ export default function CharacterCreator({ onPlayerCreate }: Props) {
                               </CarouselItem>
                           ))}
                       </CarouselContent>
-                      <CarouselPrevious />
-                      <CarouselNext />
+                      <CarouselPrevious variant="ghost" className="absolute -left-4 top-1/2 -translate-y-1/2 z-10 bg-background/50 hover:bg-background/80 text-foreground" />
+                      <CarouselNext variant="ghost" className="absolute -right-4 top-1/2 -translate-y-1/2 z-10 bg-background/50 hover:bg-background/80 text-foreground" />
                   </Carousel>
               </div>
 
@@ -235,8 +235,8 @@ export default function CharacterCreator({ onPlayerCreate }: Props) {
                                )
                           })}
                       </CarouselContent>
-                      <CarouselPrevious />
-                      <CarouselNext />
+                      <CarouselPrevious variant="ghost" />
+                      <CarouselNext variant="ghost" />
                   </Carousel>
               </div>
 
